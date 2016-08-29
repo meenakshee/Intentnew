719c39fceed29d923cda5cd2ab4429653d8044ad
meenakshee <durby1892@gmail.com>
2016-08-28 23:46:02 -0700

    made  some changes

--- app/src/main/java/com/example/minaxipc/intentnew/MainActivity.java
+++ app/src/main/java/com/example/minaxipc/intentnew/MainActivity.java
@@ -6,13 +6,17 @@ import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
+    TextView textView;
+    public static final int REQUEST_CODE_FUNCTION = 100;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        textView=(TextView)findViewById(R.id.tv1);
         final EditText metdata=(EditText)findViewById(R.id.editText);
         final EditText metdata1=(EditText)findViewById(R.id.editText1);
         Button mbtpass=(Button)findViewById(R.id.btnpass);
@@ -23,8 +27,25 @@ public class MainActivity extends AppCompatActivity {
                 intent.putExtra("data",metdata.getText().toString());
                 int age = Integer.parseInt(metdata1.getText().toString());
                 intent.putExtra("age",age);
-                startActivity(intent);
+                startActivityForResult(intent,REQUEST_CODE_FUNCTION);
             }
         });
     }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+
+        if (requestCode==REQUEST_CODE_FUNCTION)
+        {
+            if (requestCode==RESULT_OK) {
+                int roll_no = data.getIntExtra("roll no", -1);
+                if (-1 == roll_no) {
+                    textView.setText("My roll_no" + roll_no + ".");
+                }
+            }
+        }
+            super.onActivityResult(requestCode, resultCode, data);
+    }
 }
+
+
